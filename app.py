6e8b553a3586e287bb4e773ab854ec58ee798823
Raymond-Blok <raymond-blok@outlook.com>
2021-04-09 06:03:57 +0200

    added database functionality

--- app.py
+++ app.py
@@ -1,12 +1,70 @@
-from flask import Flask, jsonify, abort, make_response
-from flask_restful import Api, Resource, reqparse, fields, marshal
-from handlers.RoomHandler import RoomHandler, RoomsHandler
+from flask import Flask, jsonify, abort, make_response, _app_ctx_stack
+from flask_restful import Api, Resource, reqparse, fields, marshal, request
+from sqlalchemy.orm import scoped_session
+import json
+
+import models
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
 
 app = Flask(__name__)
 api = Api(app)
 
+app.session = scoped_session(SessionLocal, scopefunc=_app_ctx_stack.__ident_func__)
+class RoomHandler2(Resource):
+	def get(self, id):
+		roomModel = app.session.query(models.Room).get(id)
+		if roomModel == None:
+			return 'error no room found', 404
+		room = {
+			'id': roomModel.id,
+			'name': roomModel.name,
+			'width': roomModel.width,
+			'length': roomModel.length,
+			'height': roomModel.height,
+			'sensors': roomModel.sensors
+		}
+		return jsonify(room)
+class RoomHandler(Resource):
+	
+	def get(self):
+		pass
+	
+	def post(self):
+		data = json.loads(request.data)
+		room = models.Room(name = data['name'],
+		width = data['width'],
+		length = data['length'],
+		height = data['height'])
+		app.session.add(room)
+		app.session.commit()
+		print('name: ' +data['name'])
+		print('width: '+data['width'])
+		print('length: '+data['length'])
+		print('height: '+data['height'])
+		return 'success',201
+
+class RoomsHandler(Resource):
+
+	def get(self):
+		rooms = app.session.query(models.Room).all()
+		roomsList = []
+		for room in rooms:
+			roomsList.append({
+				'id': room.id,
+				'name': room.name,
+				'width': room.width,
+				'length': room.length,
+				'height': room.height,
+				'sensors': room.sensors
+			})
+		return jsonify(roomsList)
 
 api.add_resource(RoomsHandler, '/room/all')
+api.add_resource(RoomHandler, '/room')
+api.add_resource(RoomHandler2, '/room/<int:id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.types import Date
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Room(Base):
+	__tablename__ = "Rooms"
+
+	id = Column(Integer, primary_key=True, index=True)
+	name = Column(String)
+	width = Column(Integer)
+	length = Column(Integer)
+	height = Column(Integer)
+	sensors = relationship("Sensor", backref="Room")
+
+class Sensor(Base):
+	__tablename__ = "Sensors"
+
+	id = Column(Integer, primary_key=True, index=True)
+	room_id = Column(Integer, ForeignKey('Rooms.id'))
+	x = Column(Integer)
+	y = Column(Integer)
+	z = Column(Integer)
